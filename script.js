5db306036fd9efd12f4b369d10e22172dfb70b30
Mathias Hidalgo <mejhc2307lbc@gmail.com>
2025-05-05 15:07:41 -0500

    Solved

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getHumanChoice() {
-    const hChoice = prompt('0 : rock, 1 : paper, 2 : scissors')
+    const hChoice = Math.floor(prompt('0 : rock, 1 : paper, 2 : scissors'))
     return hChoice < 1 ? 'rock' : hChoice < 2 ? 'paper' : 'scissors';
 }
 
@@ -11,26 +11,58 @@ function getComputerChoice() {
     return cChoice < 1 ? 'rock' : cChoice < 2 ? 'paper' : 'scissors';
 }
 
+function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
     if(humanChoice == computerChoice){
+        humanScore++;
+        computerScore++;
         return console.log('draw!, try again refreshing')
     }
-    if(humanChoice > computerChoice &&  humanChoice == 'scissors') {
-        return console.log(`You lost! ${computerChoice} beats ${humanChoice}`) 
+    if(humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        return console.log(`You lost! ${computerChoice} beats ${humanChoice}`)
     }
-    if(computerChoice > humanChoice && computerChoice == 'scissors' ) {
+    if(computerChoice == "scissors" && humanChoice == "rock") {
+        humanScore++;
         return console.log(`You won! ${humanChoice} beats ${computerChoice}`) 
     }
-    if(humanChoice < computerChoice && humanChoice == 'rock') {
+    if(humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        return console.log(`You won! ${humanChoice} beats ${computerChoice}`) 
+    }
+    if(computerChoice == "scissors" && humanChoice == "paper") {
+        computerScore++;
         return console.log(`You lost! ${computerChoice} beats ${humanChoice}`) 
     }
-    if(computerChoice < humanChoice && computerChoice == 'rock' ) {
+
+    if(computerChoice == "paper" && humanChoice == "rock") {
+        computerScore++;
+        return console.log(`You lost! ${computerChoice} beats ${humanChoice}`) 
+    }
+    if(humanChoice == "paper" && computerChoice == "rock" ) {
+        humanScore++;
         return console.log(`You won! ${humanChoice} beats ${computerChoice}`)
 }
 
-}
-const humanElection = getHumanChoice();
-const computerElection = getComputerChoice();
 
-playRound(humanElection, computerElection);
+
+}
+
+function playgame() {;
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    console.log(`You won ${humanScore} times and lost ${computerScore} times`)
+    if(humanScore > computerScore){
+        console.log('You are the winner!')
+    }
+}
+
+
+
+
+playgame();
